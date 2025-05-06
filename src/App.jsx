04bb95c1ba3d72de04a63913762ee9f353ff418f
mostafarakib <mostafa.rakib.10@gmail.com>
2025-05-06 11:24:42 +0600

    configured redux for auth services

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <h1>App with appwrite</h1>
-    </>
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="block w-full">
+        <Header />
+        <main>todo: {/* <Outlet/> */}</main>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import config from "../config";
+import config from "../config/config.js";
 
 import { Client, Account, ID } from "appwrite";
 
