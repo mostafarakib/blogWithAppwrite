658d57f751f042b6f976de27c444cb201843d9c5
mostafarakib <mostafa.rakib.10@gmail.com>
2025-05-10 12:38:07 +0600

    configured btn, input and header component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo/Logo";
+import LogoutBtn from "./Footer/LogoutBtn";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn };
