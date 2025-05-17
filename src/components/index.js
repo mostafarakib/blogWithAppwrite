f62cfa359128db7bc67505e57898257724ed1dbe
mostafarakib <mostafa.rakib.10@gmail.com>
2025-05-18 02:36:35 +0600

    configured login and signup component with functionality

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
-import Logo from "./Logo/Logo";
-import LogoutBtn from "./Footer/LogoutBtn";
+import Logo from "./UI/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export { Header, Footer, Container, Logo, LogoutBtn };
